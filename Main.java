e73533db260452ef7ef65a96962a7c4da8d1c8d6
Kundeti Rohit Chowdary <rohitchinna365@gmail.com>
2020-05-11 15:15:59 +0000

    oooooo

--- Main.java
+++ Main.java
@@ -4,23 +4,9 @@ public class Main {
 
   public static int getLargestNumber(int number) {
     
-    int[][] table = new int[number][number];
-    
-    for(int i=0; i<number; i++) {
-      table[i][0] = 1;
-    }
-    
-    for(int j=0; j<number; j++) {
-      table[0][j] = 1;
-    }
-    
-    for(int i=1; i<number; i++) {
-      for(int j=1; j<number; j++) {
-        table[i][j] = table[i-1][j] + table[i][j-1];
-      }
-    }
+    int[] table = new int[]{1, 2, 6, 20, 70, 252, 924, 3432, 12870, 48620};
 
-    return table[number-1][number-1];
+    return table[number-1];
   }
 
   public static void main(String[] args) {
